ecc9fa13bd17889771d2cc62f6cb6d1fe895a342
Karina <habibullinak910@gmail.com>
2025-04-30 12:16:43 +0300

    UpdateID сделан

--- src/commands/UpdateID.java
+++ src/commands/UpdateID.java
@@ -1,9 +1,8 @@
 package commands;
 import basic.*;
-import commands.base.Command;
-
-import java.util.ArrayDeque;
+import commands.base.*;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class UpdateID extends Command {
     private UpdateID() {
@@ -11,14 +10,27 @@ public class UpdateID extends Command {
     }
 
     @Override
-    public void execute(ArrayDeque<Person> arDeq) throws NullException {
-        arDeq.poll();
+    public void execute(Environment env) throws NullException {
+        Iterator<Person> iterator = env.profiles.iterator();
+        FieldsWork fw = new FieldsWork();
+        long id = fw.id();
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            if (person.getId() == id) {
+                float height = fw.height();
+                person.setHeight(height);
+                System.out.println("Рост успешно изменен");
+            }else {
+                System.out.println("Не удалось изменить рост");
+            }
+        }
+        System.out.println("Процесс удаления по росту выполнен");
     }
 
 
     @Override
     public String getHelp(){
-        return "Обновить значение элемента коллекции, id которого вы ввели";
+        return "Обновить значение элемента коллекции по id";
     }
 
     public static void register(HashMap<String, Command> stringCommandHashMap) {
