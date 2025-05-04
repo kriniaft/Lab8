be7fca0ae151b337969adc5fa6aebf41477e2926
Karina <habibullinak910@gmail.com>
2025-05-05 00:11:29 +0300

    я подключила мавен к проекту, вроде как это не локально, но крайне важно для сборки дальше

--- src/fileWork/FileController.java
+++ src/fileWork/FileController.java
@@ -1,4 +1,10 @@
 package fileWork;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.io.*;
 
 public class FileController {
+    public void fileReader(){
+
+    }
 }
